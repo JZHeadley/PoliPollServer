7599ec2de66211537a483cd4ad3e4d70103fc62b
J. Zephyr Headley <headleyjz@vcu.edu>
2018-03-04 07:25:52 -0500

    forgot to make it an @RestController

--- src/main/java/com/jzheadley/polipoll/server/rest/controllers/FirebaseMessagingController.kt
+++ src/main/java/com/jzheadley/polipoll/server/rest/controllers/FirebaseMessagingController.kt
@@ -1,7 +1,6 @@
 package com.jzheadley.polipoll.server.rest.controllers
 
 import com.jzheadley.polipoll.server.models.Bill
-import com.jzheadley.polipoll.server.models.MessagingId
 import com.jzheadley.polipoll.server.repositories.MessagingIdRepository
 import de.bytefish.fcmjava.client.FcmClient
 import de.bytefish.fcmjava.model.options.FcmMessageOptions
@@ -9,17 +8,17 @@ import de.bytefish.fcmjava.requests.data.DataMulticastMessage
 import de.bytefish.fcmjava.requests.notification.NotificationPayload
 import de.bytefish.fcmjava.responses.FcmMessageResponse
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import java.sql.Date
 import java.time.Duration
 
+@RestController
 class FirebaseMessagingController(private val messagingIdRepo: MessagingIdRepository, private val fcmClient: FcmClient) {
-
-    @PostMapping("messagingIds")
-    fun storeMessagingId(@RequestBody messagingId: String) {
-        messagingIdRepo.save(MessagingId(messagingId))
-    }
+//
+//    @PostMapping("messagingIds")
+//    fun storeMessagingId(@RequestBody messagingId: String) {
+//        messagingIdRepo.save(MessagingId(messagingId))
+//    }
 
     @GetMapping("test/messagingIds/send")
     fun testNotificationSending() {
