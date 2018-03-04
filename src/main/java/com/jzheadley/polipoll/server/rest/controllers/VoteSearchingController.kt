34192e29ff9a416dba0014222b6e40bba4632f52
J. Zephyr Headley <headleyjz@vcu.edu>
2018-03-04 10:39:06 -0500

    wrong path thing

--- src/main/java/com/jzheadley/polipoll/server/rest/controllers/VoteSearchingController.kt
+++ src/main/java/com/jzheadley/polipoll/server/rest/controllers/VoteSearchingController.kt
@@ -5,13 +5,13 @@ import com.google.gson.GsonBuilder
 import com.jzheadley.polipoll.server.repositories.VoteRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam
 
 @RestController
 class VoteSearchingController(private val voteRepo: VoteRepository, private val gson: Gson) {
     @GetMapping("votes/stats/{race}")
-    fun getNumYesNoByRace(@PathParam("race") race: String): ResponseEntity<String>? {
+    fun getNumYesNoByRace(@PathVariable("race") race: String): ResponseEntity<String>? {
         var numYes = voteRepo.numOfYesByRace(race)
         var numNo = voteRepo.numOfNoByRace(race)
         var gson: Gson = GsonBuilder().create()
